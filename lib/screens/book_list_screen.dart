ff189c70e88ef83d6953bbe1685547d021eba223
TedySyach <tedyysyyach@gmail.com>
2022-11-19 19:44:48 +0700

    [Update] Membuat data dari API dapat di lihat di aplikasi

--- lib/screens/book_list_screen.dart
+++ lib/screens/book_list_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:tugas_2_bootcamp/models/book_list_respone.dart';
 
 class BookListScreen extends StatefulWidget {
   const BookListScreen({super.key});
@@ -11,6 +12,9 @@ class BookListScreen extends StatefulWidget {
 }
 
 class _BookListScreenState extends State<BookListScreen> {
+  // variable public
+  BookListRespone? booklist;
+
   //Fungsi untuk mengambil data dari API
   fetchBookApi() async {
     var url = Uri.parse('https://api.itbook.store/1.0/new');
@@ -21,7 +25,11 @@ class _BookListScreenState extends State<BookListScreen> {
     print('Response body: ${response.body}');
 
     //Mengubah respone dari body yang berupa string kedalam Json
-    final jsonBookList = jsonDecode(response.body);
+    if (response.statusCode == 200) {
+      final jsonBookList = jsonDecode(response.body);
+      booklist = BookListRespone.fromJson(jsonBookList);
+      setState(() {});
+    }
   }
 
   //Melakukan Pemanggilan Function fectBookApi
@@ -39,6 +47,46 @@ class _BookListScreenState extends State<BookListScreen> {
         title: const Text(
           "Daftar Buku",
         ),
+      ),
+      body: Container(
+        child: (booklist == null)
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.builder(
+                itemCount: booklist!.books!.length,
+                itemBuilder: ((context, index) {
+                  final currentBook = booklist!.books![index];
+                  return Row(
+                    children: [
+                      Image.network(
+                        currentBook.image!,
+                        height: 100,
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(currentBook.title!),
+                              Text(currentBook.subtitle!),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Text(
+                                  currentBook.price!,
+                                  style: const TextStyle(
+                                    color: Colors.green,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                })),
       ),
     );
   }
